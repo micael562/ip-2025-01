ceab1a63457f5c323023ab9c884da4a2078fce37
micael562 <159570185+micael562@users.noreply.github.com>
2025-04-11 18:36:32 -0300

    Create b0.go

---
+++ teste01/b0.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	fmt.Print("Digite um número inteiro maior que 1: ")
+	fmt.Scan(&x)
+
+	if x > 1 {
+		fmt.Printf("%d pratos de trigo pra %d tigres tristes\n", x, x)
+	} else {
+		fmt.Println("O número deve ser maior que 1.")
+	}
+}
