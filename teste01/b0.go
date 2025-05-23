ca3223e24379051847b0666bc9e7d981a7b1f390
micael562 <159570185+micael562@users.noreply.github.com>
2025-05-23 12:41:50 -0300

    Update b0.go

--- teste01/b0.go
+++ teste01/b0.go
@@ -4,12 +4,11 @@ import ("fmt")
 
 func main() {
 	var x int
-	fmt.Print("Digite um número inteiro maior que 1: ")
 	fmt.Scan(&x)
 
 	if x > 1 {
-		fmt.Printf("%d pratos de trigo pra %d tigres tristes\n", x, x)
+		fmt.Printf("%d pratos de trigo pra %d tigres tristes", x, x)
 	} else {
-		fmt.Println("O número deve ser maior que 1.")
+		return
 	}
 }
