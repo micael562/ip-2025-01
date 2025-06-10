cf7e45306a449c09bc4d74c0f0ab43bf39bf050a
micael562 <159570185+micael562@users.noreply.github.com>
2025-06-09 22:39:34 -0300

    Update c7.go

--- teste01/c7.go
+++ teste01/c7.go
@@ -8,21 +8,17 @@ func main() {
 	var x float64
 	fmt.Scan(&x)
 
-	s := 0.0
-	potencia := 1.0  
+	s := x            
+	potencia := x    
 	fatorial := 1.0  
-	sinal := 1.0   
+	sinal := -1.0    
 
-	for i := 0; i < 20; i++ {
-		if i == 0 {
-			s += x 
-			potencia = x
-		} else {
-			potencia *= x
-			fatorial *= float64(i)
-			s += sinal * potencia / fatorial
-		}
-		sinal *= -1
+	for i := 1; i < 20; i++ {
+		fatorial *= float64(i)
+		termo := sinal * potencia / fatorial
+		s += termo
+		potencia *= x     
+		sinal *= -1    
 	}
 
 	fmt.Printf("%.5f\n", s)
