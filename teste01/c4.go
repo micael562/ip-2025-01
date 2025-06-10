8abca712d1a9277f850297cdffb09fad14c0d195
micael562 <159570185+micael562@users.noreply.github.com>
2025-06-09 21:55:10 -0300

    Update c4.go

--- teste01/c4.go
+++ teste01/c4.go
@@ -1,1 +1,30 @@
+package main
 
+import ("fmt")
+
+func isQuadradoPerfeito(n int) bool {
+	if n < 1 {
+		return false
+	}
+	for i := 1; i*i <= n; i++ {
+		if i*i == n {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	var num int
+	for {
+		fmt.Scan(&num)
+		if num <= 0 {
+			break
+		}
+		if isQuadradoPerfeito(num) {
+			fmt.Printf("%d eh quadrado perfeito\n", num)
+		} else {
+			fmt.Printf("%d nao eh quadrado perfeito\n", num)
+		}
+	}
+}
